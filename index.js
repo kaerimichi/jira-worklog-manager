6f049746912ec48a6d239e9acf3efded0deaa8e1
Gabriel Rimes <jivagorimes@gmail.com>
2018-08-31 18:05:15 -0300

    Update error handling

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 require('dotenv').config()
 
 const program = require('commander')
+const { run } = require('./lib/commandRunner')
 
-program.version('1.0.10')
+program.version(require('./package.json').version)
 
 program
   .command('check')
   .option('-i, --issue [issue]', 'issue identifier')
-  .action(options => require('./lib/commands/check')(options))
+  .action(options => run('check', options))
 
 program
   .command('register')
@@ -17,12 +18,12 @@ program
   .option('-s, --start [start]', 'start time (HH:mm)')
   .option('-e, --end [end]', 'end time (HH:mm)')
   .option('-i, --issue [issue]', 'issue identifier')
-  .action(options => require('./lib/commands/register')(options))
+  .action(options => run('register', options))
 
 program
   .command('bulk-register')
   .option('-f, --filename [filename]', 'path to a YAML file with the worklogs')
   .option('--dry-run', 'list work logs to be registered without actually registering them')
-  .action(options => require('./lib/commands/bulkRegister')(options))
+  .action(options => run('bulkRegister', options))
 
 program.parse(process.argv)
